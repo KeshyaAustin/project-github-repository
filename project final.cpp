8abd2316f0e08e4c42bba658029c7915548a6e2a
Ke'Shya Austin-Cephus <97460847+KeshyaAustin@users.noreply.github.com>
2022-04-28 11:19:40 -0500

    Add files via upload

---
+++ project final.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+class Person {
+
+	private:
+    "String Type";
+		"Get Name";
+		int: id
+			Public:
+				void SetClassification(ifstream);
+				void setid(ifstream c);
+[ResidentBehavior];
+ResidentBehavior(StringType; Response: string)
+
+String: Attitude;
+
+String: aggressive;
+
+String: disruptive;
+			
+};
+{
+	class of name: public Person
+}
+Bias;
+public
+void setid(string id);
+void getBias(ifstream);
+void print() const:
+void setClass(int)
+{
+
+ [Class Resident Discrimination];
+
+StringType: race;
+
+String: color ;
+
+String: age ;
+
+String: gender;
+
+GetRace;
+
+Getcolor;
+
+Getage;
+
+Int: gender?;
+};
+{
+Class:	Resident Bais;
+
+Stringtype: Diversity;
+
+String: different;
+
+String: exclude;
+
+String: rights;
+
+Getdiversity;
+
+Getdifferent;
+
+Getexclude;
+
+Int: right ;
+}
+<<"====="
+<<endl;
+cout<<"+++" menu
+<<endl; 
+	return 0;
+
+
+
+
